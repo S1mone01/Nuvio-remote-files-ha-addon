e10e4d5459ad3863bc77ac073e3cc400d603e1ae
S1mone01 <simonesaluto@gmail.com>
2026-05-30 11:55:45 +0200

    feat: add support for disk mounting in HA addon

--- stremio-remote-files/run.sh
+++ stremio-remote-files/run.sh
@@ -12,6 +12,36 @@ SERIES_DIR=$(bashio::config 'series_dir_name')
 MEDIA_DISK_NAME=$(bashio::config 'media_disk_name')
 SUBPATH=$(bashio::config 'media_subpath')
 
+# Optional Disk Mounting
+if bashio::config.has_value 'mount_device'; then
+    MOUNT_DEVICE=$(bashio::config 'mount_device')
+    MOUNT_PATH=$(bashio::config 'mount_path')
+    
+    # If mount_path is empty, use current media_disk_name
+    if [ -z "$MOUNT_PATH" ]; then
+        MOUNT_PATH="${MEDIA_DISK_NAME}"
+    fi
+
+    # We enforce mounting under /media because the app is hardcoded to look there
+    if [[ ! "$MOUNT_PATH" =~ ^/media/ ]]; then
+        # Remove leading slash if any to append to /media/
+        CLEAN_PATH=$(echo "${MOUNT_PATH}" | sed 's|^/||')
+        MOUNT_PATH="/media/${CLEAN_PATH}"
+    fi
+
+    bashio::log.info "Tentativo di montaggio: ${MOUNT_DEVICE} su ${MOUNT_PATH}..."
+    mkdir -p "${MOUNT_PATH}"
+    
+    if mount "${MOUNT_DEVICE}" "${MOUNT_PATH}"; then
+        bashio::log.info "Montaggio completato con successo."
+        # Update MEDIA_DISK_NAME to be relative to /media for the app
+        MEDIA_DISK_NAME=$(echo "${MOUNT_PATH}" | sed 's|/media/||')
+        bashio::log.info "MEDIA_DISK_NAME aggiornato a: ${MEDIA_DISK_NAME}"
+    else
+        bashio::log.error "Impossibile montare ${MOUNT_DEVICE}. Verifica i permessi, il formato del disco o il percorso."
+    fi
+fi
+
 # Get Local IP (using hostname -i for BusyBox compatibility)
 INTERNAL_IP=$(hostname -i | awk '{print $1}')
 MEDIA_BASE_URL="http://${INTERNAL_IP}:${PORT}"
